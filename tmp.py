b22f2d1f9a4c168738ddf17b08069a74fd94a79f
SillyPasty <47266819+SillyPasty@users.noreply.github.com>
2020-06-12 09:20:15 +0800

    Ex19

--- tmp.py
+++ tmp.py
@@ -8,5 +8,11 @@ def foo2(x):
     res = 125 * (x**2) - (10 / 3) * (x**3)
     return res
 
-print(foo1(x) - foo1(y))
-print(foo2(20))
+def octal_int(s):
+    if len(s) == 1:
+        return int(s[0])
+    else:
+        return 8 * octal_int(s[:-1]) + int(s[-1])
+
+
+print(octal_int('3'))
---
+++ twopointers/Ex19.java
@@ -0,0 +1,19 @@
+public class Ex19 {
+
+}
+
+class Ex19Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode rp = head, lp = head;
+        for (int i = 0; i < n; i++)
+            rp = rp.next;
+        if (rp == null)
+            return head.next;
+        while (rp.next != null) {
+            lp = lp.next;
+            rp = rp.next;
+        }
+        lp.next = lp.next.next;
+        return head;
+    }
+}
