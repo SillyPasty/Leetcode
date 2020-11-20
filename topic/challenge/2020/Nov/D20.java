85b7ad0d4936c7e485f059a7f1ac0762fd7940ec
SillyPasty <SillyPasty@users.noreply.github.com>
2020-11-20 21:36:46 +0800

    dcd20

--- topic/challenge/2020/Nov/D20.java
+++ topic/challenge/2020/Nov/D20.java
@@ -10,35 +10,38 @@ public class ListNode {
 class Solution {
     public ListNode insertionSortList(ListNode head) {
         int idx = 1;
-        ListNode cur = head;
-        ListNode curPrev = head;
         if (head == null || head.next == null)
             return head;
+        ListNode cur = head.next;
+        ListNode curPrev = head;
         while (cur != null) {
             int val = cur.val;
             ListNode tmp = head;
-            ListNode tmpPrev = head;
+            ListNode tmpPrev = null;
             ListNode next = cur.next;
             for (int i = 0; i < idx; i++) {
-                if (tmp.val > val) {
+                if (tmp == null || tmp.val > val) {
                     break;
-                } else {
-                    tmpPrev = tmp;
-                    tmp = tmp.next;
                 }
+                tmpPrev = tmp;
+                tmp = tmp.next;
             }
-            if (tmp == head) {
+            if (tmp == cur) {
+                cur = cur.next;
+                curPrev = curPrev.next;
+            } else if (tmp == head) {
                 curPrev.next = cur.next;
                 cur.next = tmp;
                 head = cur;
                 cur = next;
             } else {
                 curPrev.next = cur.next;
-                cur.next = tmp;
+                cur.next = tmpPrev.next;
                 tmpPrev.next = cur;
                 cur = next;
             }
             idx++;
+            ListNode tra = head;
         }
         return head;
     }
