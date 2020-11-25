5bc5b0a466eee218e63fa32769de5ebf879a24e7
SillyPasty <1179041704@qq.com>
2020-11-25 22:48:07 +0800

    dcd25

---
+++ topic/challenge/2020/Nov/D24.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countNodes(TreeNode* root) {
+        if (!root)
+            return 0;
+        int tmp = 1;
+        tmp += countNodes(root->left);
+        tmp += countNodes(root->right);
+        return tmp;
+    }
---
+++ topic/challenge/2020/Nov/D25.cpp
@@ -0,0 +1,30 @@
+string sortString(string s)
+{
+    const int len = s.size();
+    int bucket[26] = {0};
+    for (char c : s)
+        ++bucket[c - 'a'];
+    string res;
+    while (1)
+    {
+        for (int i = 0; i < 26 && res.size() < len; i++)
+        {
+            if (bucket[i])
+            {
+                res.push_back('a' + i);
+                --bucket[i];
+            }
+        }
+        for (int i = 25; i >= 0 && res.size() < len; i--)
+        {
+            if (bucket[i])
+            {
+                res.push_back('a' + i);
+                --bucket[i];
+            }
+        }
+        if (res.size() >= len)
+            break;
+    }
+    return res;
+}
