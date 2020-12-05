ccd0ce457d7b8d747414803317c360b1091c23ca
SillyPasty <1179041704@qq.com>
2020-12-05 12:26:45 +0800

    dcd5_better

--- topic/challenge/2020/Dec/D5.cpp
+++ topic/challenge/2020/Dec/D5.cpp
@@ -2,27 +2,41 @@
 using namespace std;
 
 int leastInterval(vector<char>& tasks, int n) {
-    int timer[26], cnt = 0;
+    int timer[26], task_cnt[26] = {0}, cnt = 0;
+    int size = tasks.size();
     for (int i = 0; i < 26; ++i)
         timer[i] = -1;
-    map<char, int> task_cnt;
     for (auto c : tasks)
-        ++task_cnt[c];
-    while (task_cnt.size()) {
+        ++task_cnt[c - 'A'];
+    while (size) {
         char cur = ' ';
-        for (auto iter = task_cnt.begin(); iter != task_cnt.end(); ++iter) {
-            if (timer[iter->first - 'A'] == -1 || cnt - timer[iter->first - 'A'] > n) {
-                cur = iter->first;
-                break;
+        int max_cnt = 0;
+        for (int i = 0; i < 26; ++i) {
+            if (task_cnt[i] && (timer[i] == -1 || cnt - timer[i] > n)) {
+                if (task_cnt[i] > max_cnt ||
+                    (task_cnt[i] == max_cnt && timer[i] < timer[cur - 'A'])) {
+                    max_cnt = task_cnt[i];
+                    cur = i + 'A';
+                }
             }
         }
         if (cur != ' ') {
-            --task_cnt[cur];
+            --task_cnt[cur - 'A'];
+            --size;
             timer[cur - 'A'] = cnt;
-            if (!task_cnt[cur])
-                task_cnt.erase(cur);
         }
         ++cnt;
     }
     return cnt;
+}
+int leastInterval2(vector<char>& tasks, int n) {
+    int len = tasks.size();
+    vector<int> vec(26);
+    for (char c : tasks)
+        ++vec[c - 'A'];
+    sort(vec.rbegin(), vec.rend());
+    int cnt = 1;
+    while (cnt < vec.size() && vec[cnt] == vec[0])
+        cnt++;
+    return max(len, cnt + (n + 1) * (vec[0] - 1));
 }
