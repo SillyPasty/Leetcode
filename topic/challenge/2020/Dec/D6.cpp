3cc89dbbdb01a7dd12276abeff3e5e6fbb7549f6
SillyPasty <1179041704@qq.com>
2020-12-06 13:20:05 +0800

    dcd6

---
+++ topic/challenge/2020/Dec/D6.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> res(numRows);
+    if (numRows == 0)
+        return res;
+    res[0].push_back(1);
+    for (int i = 1; i < numRows; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            if (j == 0 || j == i)
+                res[i].push_back(1);
+            else
+                res[i].push_back(res[i - 1][j] + res[i - 1][j - 1]);
+        }
+    }
+    return res;
+}
